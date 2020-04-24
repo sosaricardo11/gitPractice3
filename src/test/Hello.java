c6acc1f384a24bee8c0e3015ea0bd44e26799e81
sosaricardo11 <64181239+sosaricardo11@users.noreply.github.com>
2020-04-24 16:28:27 -0400

    Update Hello.java

--- src/test/Hello.java
+++ src/test/Hello.java
@@ -3,6 +3,6 @@ package test;
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("hello futer ricardo");
     }
 }
